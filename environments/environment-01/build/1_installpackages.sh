fcf0be77139b9358ba4f83a4154ee23cb205cf4f
Wendell Odom <10836358+WendellOdom@users.noreply.github.com>
2021-03-08 11:37:03 -0500

    various issues w/ commands to install ansible onto Kata base Ubuntu 2004 image.

--- environments/environment-01/build/1_installpackages.sh
+++ environments/environment-01/build/1_installpackages.sh
@@ -1,4 +1,8 @@
 apt-get update
 apt-get install traceroute
 apt-get install curl
+# Katacoda base image supplies Ansible 2.9.something, installed by apt.
+# I want a later release, installed by pip.
+# Next step removes base image ansible.
+apt remove ansible
 
--- environments/environment-01/build/2_pip-installs.sh
+++ environments/environment-01/build/2_pip-installs.sh
@@ -1,2 +1,3 @@
+pip install upgrade pip
 pip install ansible==2.10
 pip install paramiko
