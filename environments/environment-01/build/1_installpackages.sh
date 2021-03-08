ea03127ac5d0eeed2c0e8c72831c358fbbda00f4
Wendell Odom <10836358+WendellOdom@users.noreply.github.com>
2021-03-08 12:00:07 -0500

    changed to apt remove -y to avoid the y/n prompt - in kata build script

--- environments/environment-01/build/1_installpackages.sh
+++ environments/environment-01/build/1_installpackages.sh
@@ -4,5 +4,5 @@ apt-get install curl
 # Katacoda base image supplies Ansible 2.9.something, installed by apt.
 # I want a later release, installed by pip.
 # Next step removes base image ansible.
-apt remove ansible
+apt remove -y ansible
 
