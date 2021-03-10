c74d1bcd344b0f1cd215bcc86c348382d0911bb7
Wendell Odom <10836358+WendellOdom@users.noreply.github.com>
2021-03-10 08:51:47 -0500

    Moved logic for loading lab-specific files to a script to be run by user once in lab

--- environments/environment-01/build/4_run_at_scenario_start.sh
+++ environments/environment-01/build/4_run_at_scenario_start.sh
@@ -1,5 +1,12 @@
-# cat << EOF > /opt/configure-environment.sh
+cat << EOF > /opt/configure-environment.sh
 #!/bin/bash
 echo 'this is a test'
+mkdir ~/netauto/ansible-course-01 -p
+cd ~/netauto/ansible-course-01
+# Curl commands rely on earlier build script to **apt-get install curl**
+curl https://www.certskills.com/safari/ansible-course-01/ansible-hosts.ini -o ansible-hosts.ini -s
+curl https://www.certskills.com/safari/ansible-course-01/ansible.cfg -o ansible.cfg -s
+curl https://www.certskills.com/safari/ansible-course-01/testpb01.yml -o testpb01.yml -s
+
 EOF
 chmod +x /opt/configure-environment.sh
